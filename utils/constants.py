0bba83fac3103f5042ac09347ac3682591c569b9
Lê Hữu Thái <lehuuthai@gmail.com>
2025-10-29 09:09:47 +0700

    update

---
+++ utils/constants.py
@@ -0,0 +1,17 @@
+# Đây là nội dung cho file: utils/constants.py
+
+# Kích thước cửa sổ
+WIDTH = 800
+HEIGHT = 800
+
+# Kích thước bàn cờ (ví dụ 8x8)
+ROWS = 8
+COLS = 8
+SQUARE_SIZE = WIDTH // COLS # Tự động tính kích thước ô cờ
+
+# Tốc độ game
+FPS = 60
+
+# Màu sắc (ví dụ)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
